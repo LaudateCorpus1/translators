b340740c632ee34351e2322ee858205e4b44a716
Jens H. Nielsen <jenshnielsen@gmail.com>
2012-08-10 11:04:56 +0200

    Add option for BibTeX export to use journal abbrevations

--- BibTeX.js
+++ BibTeX.js
@@ -9,7 +9,8 @@
 	"displayOptions": {
 		"exportCharset": "UTF-8",
 		"exportNotes": true,
-		"exportFileData": false
+		"exportFileData": false,
+		"useJournalAbbreviation": false
 	},
 	"inRepository": true,
 	"translatorType": 3,
@@ -2105,6 +2106,8 @@ function doExport() {
 		if(item.publicationTitle) {
 			if(item.itemType == "bookSection" || item.itemType == "conferencePaper") {
 				writeField("booktitle", item.publicationTitle);
+			} else if(Zotero.getOption("useJournalAbbreviation")){
+				writeField("journal", item.journalAbbreviation);
 			} else {
 				writeField("journal", item.publicationTitle);
 			}
